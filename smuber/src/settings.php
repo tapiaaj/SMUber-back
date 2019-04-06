03c447501fc3bc18e7ddadc24f28a3cf5ea761d6
Julie Tapia <jtapia@smu.edu>
2019-04-06 16:45:24 -0500

    smuber

--- smuber/src/settings.php
+++ smuber/src/settings.php
@@ -15,5 +15,13 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+
+        // Database connection settings
+        "db" => [
+            "host" => "127.0.0.1:3306",
+            "dbname" => "smuber",
+            "user" => "smuber",
+            "pass" => "smuber"
+        ],
     ],
 ];
